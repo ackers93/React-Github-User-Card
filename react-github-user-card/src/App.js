9330f1d783cee994ae554f3e6a14841135a7e1f6
Andrew Ackerman <andrew.ackers@gmail.com>
2019-12-03 12:34:45 -0800

    Refactored add into class

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from "axios";
+import './App.css';
+
+class App extends React.Component {
+
+
+  render() {  
+    return (
+    <div className="App">
+      <h1>This is the app</h1>
+    </div>
+  );}
+
+}
+
+export default App;
